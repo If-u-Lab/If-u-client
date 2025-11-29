fa31cf963679d75d2096be82df86d5f388304291
wngktjd13 <seonguk3553@kookmin.ac.kr>
2025-11-29 23:25:06 +0900

    feat: AuthProvider 추가

--- components/client-providers.tsx
+++ components/client-providers.tsx
@@ -1,7 +1,12 @@
 "use client"
 
 import { QuestionsProvider } from "@/contexts/questions-context"
+import { AuthProvider } from "@/contexts/auth-context"
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
-  return <QuestionsProvider>{children}</QuestionsProvider>
+  return (
+    <AuthProvider>
+      <QuestionsProvider>{children}</QuestionsProvider>
+    </AuthProvider>
+  )
 }
